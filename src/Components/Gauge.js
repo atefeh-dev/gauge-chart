5d0b5d9a81b314a728c5fde59a73378d57b538b0
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-02-01 21:15:07 +0330

    add line

--- src/Components/Gauge.js
+++ src/Components/Gauge.js
@@ -2,14 +2,89 @@
 
 import React from "react";
 import "../Style/gauge.css";
+
 const Gauge = () => {
   return (
-    <>
-      <p>Gauge chart</p>
-      <div className="gauge">
-        <input type="range" min="0" max="100" value="38" />
+    <div className="gauge ">
+      <div className="units">
+        <div className="line"></div>
+        <div className="ticks">
+          <span className="tick tick--big">
+            <span>0</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>10</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>20</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>30</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>40</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>50</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>60</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>70</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>80</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>90</span>
+          </span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick"></span>
+          <span className="tick tick--big">
+            <span>100</span>
+          </span>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Gauge;
